ce25401cb3cc827d75a1921082582471bef08999
Sean Dodson <dodsonsean@comcast.net>
2021-10-16 13:06:50 -0400

    Add KTC draft pick query

---
+++ src/transaction-assets/ktc/queries/__tests__/get-by-details-ktc-draft-pick-query.test.ts
@@ -0,0 +1,70 @@
+import * as firebase from 'firebase-admin';
+import { when } from 'jest-when';
+import { GetByDetailsKtcDraftPickQuery } from '../get-by-details-ktc-draft-pick-query';
+
+describe('GetByDetailsKtcDraftPickQuery', () => {
+  let query: GetByDetailsKtcDraftPickQuery;
+
+  let mockDatabaseGet: jest.Mock;
+
+  let round: string;
+  let year: string;
+
+  beforeEach(() => {
+    round = 'round';
+    year = 'year';
+
+    mockDatabaseGet = jest.fn();
+    const mockLimitToFirst = jest.fn();
+    when(mockLimitToFirst).calledWith(1).mockReturnValue({
+      get: mockDatabaseGet,
+    });
+    const mockEqualTo = jest.fn();
+    when(mockEqualTo).calledWith('round_year').mockReturnValue({
+      limitToFirst: mockLimitToFirst,
+    });
+    const mockOrderByChild = jest.fn();
+    when(mockOrderByChild).calledWith('round_year_index').mockReturnValue({
+      equalTo: mockEqualTo,
+    });
+    const mockDatabaseRef = jest.fn();
+    when(mockDatabaseRef)
+      .calledWith('/transaction_assets/ktc/draft_picks')
+      .mockReturnValue({
+        orderByChild: mockOrderByChild,
+      });
+    const mockFirebaseApp = {
+      database: () => ({
+        ref: mockDatabaseRef,
+      }),
+    } as unknown as firebase.app.App;
+
+    query = new GetByDetailsKtcDraftPickQuery(mockFirebaseApp);
+  });
+
+  it('should return draft pick for details', async () => {
+    mockDatabaseGet.mockReturnValue({
+      val: () => ({
+        1: {
+          id: '1',
+        },
+      }),
+    });
+
+    const result = await query.execute(round, year);
+
+    expect(result).toEqual({
+      id: '1',
+    });
+  });
+
+  it('should return null if draft pick not found', async () => {
+    mockDatabaseGet.mockReturnValue({
+      val: () => null,
+    });
+
+    const result = await query.execute(round, year);
+
+    expect(result).toBeNull();
+  });
+});
---
+++ src/transaction-assets/ktc/queries/get-by-details-ktc-draft-pick-query.ts
@@ -0,0 +1,40 @@
+import * as firebase from 'firebase-admin';
+import { KtcDraftPick } from '../ktc-transaction-asset';
+import { KtcDraftPicksMapDto } from '../ktc-transaction-asset-dto';
+
+/**
+ * Query to get a KTC draft pick by the draft pick's details.
+ */
+export class GetByDetailsKtcDraftPickQuery {
+  /**
+   * Initialize with a Firebase app.
+   * @param firebaseApp The initialized Firebase app.
+   */
+  constructor(private firebaseApp: firebase.app.App) {}
+
+  /**
+   * Get a KTC draft pick by the draft pick details.
+   * @param round The round of the draft pick to query.
+   * @param year The year of the draft pick to query.
+   * @returns The draft pick for the details. Null if draft pick not found.
+   */
+  async execute(round: string, year: string): Promise<KtcDraftPick | null> {
+    const ktcDraftPicksPath = `/transaction_assets/ktc/draft_picks`;
+
+    const ktcDraftPickData = await this.firebaseApp
+      .database()
+      .ref(ktcDraftPicksPath)
+      .orderByChild('round_year_index')
+      .equalTo(`${round}_${year}`)
+      .limitToFirst(1)
+      .get();
+
+    const ktcDraftPicksMapDto: KtcDraftPicksMapDto = ktcDraftPickData.val();
+
+    if (ktcDraftPicksMapDto === null) {
+      return null;
+    }
+
+    return Object.values(ktcDraftPicksMapDto)[0];
+  }
+}
