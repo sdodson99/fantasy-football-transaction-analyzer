c6e809d0e4c5afc5a7f4bab9ae204182c25250c1
Sean Dodson <dodsonsean@comcast.net>
2021-10-19 19:49:51 -0400

    Cleanup logger

--- src/functions/update-ktc-transaction-assets/index.ts
+++ src/functions/update-ktc-transaction-assets/index.ts
@@ -1,19 +1,20 @@
 import * as functions from 'firebase-functions';
+import { logger } from '../../core/logger';
 import * as services from './services';
 
 export const updateKtcTransactionAssets = functions.storage
   .object()
   .onFinalize(async (file) => {
-    functions.logger.info('Detected Firebase Storage file creation.');
+    logger.info('Detected Firebase Storage file creation.');
 
     if (file.name !== 'ktc-players.json') {
-      return functions.logger.info(
+      return logger.info(
         'Created file was not for KTC players update. Maybe next time...',
         { fileName: file.name }
       );
     }
 
-    functions.logger.info('Starting KTC transaction assets update.');
+    logger.info('Starting KTC transaction assets update.');
 
     const serviceProvider = services.build();
 
@@ -25,26 +26,23 @@ export const updateKtcTransactionAssets = functions.storage
       serviceProvider.resolveSaveAllKtcDraftPicksCommand();
 
     try {
-      functions.logger.info('Querying all Sleeper transaction assets.');
+      logger.info('Querying all Sleeper transaction assets.');
       const { players, draftPicks } =
         await getAllKtcStoredTransactionAssetsQuery.execute();
-      functions.logger.info(
-        'Successfully queried all KTC transaction assets.',
-        {
-          ktcDraftPicksCount: draftPicks.length,
-          ktcPlayersCount: players.length,
-        }
-      );
+      logger.info('Successfully queried all KTC transaction assets.', {
+        ktcDraftPicksCount: draftPicks.length,
+        ktcPlayersCount: players.length,
+      });
 
-      functions.logger.info('Saving all KTC players.');
+      logger.info('Saving all KTC players.');
       await saveAllKtcPlayersCommand.execute(players);
 
-      functions.logger.info('Saving all KTC draft picks.');
+      logger.info('Saving all KTC draft picks.');
       await saveAllKtcDraftPicksCommand.execute(draftPicks);
 
-      functions.logger.info('Successfully updated KTC transaction assets.');
+      logger.info('Successfully updated KTC transaction assets.');
     } catch (error) {
-      functions.logger.error('Failed to update KTC transaction assets.', {
+      logger.error('Failed to update KTC transaction assets.', {
         error,
       });
     }
--- src/functions/update-sleeper-players/index.ts
+++ src/functions/update-sleeper-players/index.ts
@@ -1,10 +1,11 @@
 import * as functions from 'firebase-functions';
+import { logger } from '../../core/logger';
 import * as services from './services';
 
 export const updateSleeperPlayers = functions.pubsub
   .schedule('0 0 1 * *')
   .onRun(async () => {
-    functions.logger.info('Starting Sleeper players update.');
+    logger.info('Starting Sleeper players update.');
 
     try {
       const serviceProvider = services.build();
@@ -14,18 +15,18 @@ export const updateSleeperPlayers = functions.pubsub
       const saveAllSleeperPlayersCommand =
         serviceProvider.resolveSaveAllSleeperPlayersCommand();
 
-      functions.logger.info('Querying all Sleeper players.');
+      logger.info('Querying all Sleeper players.');
       const sleeperPlayers = await getAllSleeperApiPlayersQuery.execute();
-      functions.logger.info('Successfully queried all Sleeper players.', {
+      logger.info('Successfully queried all Sleeper players.', {
         sleeperPlayersCount: sleeperPlayers.length,
       });
 
-      functions.logger.info('Saving all Sleeper players.');
+      logger.info('Saving all Sleeper players.');
       await saveAllSleeperPlayersCommand.execute(sleeperPlayers);
 
-      functions.logger.info('Successfully updated Sleeper players.');
+      logger.info('Successfully updated Sleeper players.');
     } catch (error) {
-      functions.logger.error('Failed to update Sleeper players.', {
+      logger.error('Failed to update Sleeper players.', {
         error,
       });
     }
